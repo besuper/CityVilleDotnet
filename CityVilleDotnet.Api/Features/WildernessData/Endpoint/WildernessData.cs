4d77855cd59993e12eb069cc819f3742c1861023
besuper <16976439+besuper@users.noreply.github.com>
2025-07-14 11:23:37 +0200

    Fix trees not showing

---
+++ CityVilleDotnet.Api/Features/WildernessData/Endpoint/WildernessData.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+
+namespace CityVilleDotnet.Api.Features.WildernessData.Endpoint;
+
+internal sealed class WildernessData : Endpoint<WildernessDataRequest>
+{
+    public override void Configure()
+    {
+        Get("/assets/wilderness/WildernessData/WildernessClientData{Uid}.txt");
+    }
+
+    public override async Task HandleAsync(WildernessDataRequest req, CancellationToken ct)
+    {
+        await SendRedirectAsync("/assets/wilderness/WildernessData/WildernessClientData14.txt");
+    }
+}
+
+internal sealed record WildernessDataRequest(
+    string Uid
+);
