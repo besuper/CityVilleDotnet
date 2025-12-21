601f180379fbb9612d814656896fa4b3586355d4
besuper <16976439+besuper@users.noreply.github.com>
2025-12-22 00:36:19 +0100

    fix: update franchise name max length

--- CityVilleDotnet.Persistence/Migrations/20251221232213_UpdateFranchiseNameMaxLength.Designer.cs
+++ CityVilleDotnet.Persistence/Migrations/20251221232213_UpdateFranchiseNameMaxLength.Designer.cs
@@ -4,6 +4,7 @@ using CityVilleDotnet.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CityVilleDotnet.Persistence.Migrations
 {
     [DbContext(typeof(CityVilleDbContext))]
-    partial class CityVilleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251221232213_UpdateFranchiseNameMaxLength")]
+    partial class UpdateFranchiseNameMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -597,6 +600,9 @@ namespace CityVilleDotnet.Persistence.Migrations
                         .IsRequired()
                         .HasMaxLength(64)
                         .HasColumnType("nvarchar(64)");
+
+                    b.Property<bool>("NeverOpened")
+                        .HasColumnType("bit");
 
                     b.Property<double?>("PlantTime")
                         .HasColumnType("float");
---
+++ CityVilleDotnet.Persistence/Migrations/20251221232213_UpdateFranchiseNameMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityVilleDotnet.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFranchiseNameMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FranchiseName",
+                table: "Franchise",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FranchiseName",
+                table: "Franchise",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+        }
+    }
+}
