331657c324a9c614f7906b591d6b65981ac13dec
besuper <16976439+besuper@users.noreply.github.com>
2026-03-21 10:18:42 +0100

    fix: find building with coords instead of id in Move

--- CityVilleDotnet.Api/Services/WorldService/Move.cs
+++ CityVilleDotnet.Api/Services/WorldService/Move.cs
@@ -11,15 +11,19 @@ internal sealed class Move(CityVilleDbContext context) : AmfService<MoveRequest>
 {
     public override async Task<ASObject> HandlePacket(MoveRequest request, Guid userId, CancellationToken cancellationToken)
     {
+        var moveParams = request.MoveParams[0];
+        var originX = moveParams.OrigX;
+        var originY = moveParams.OrigY;
+
         var user = await context.Set<User>()
             .AsSplitQuery()
             .Include(x => x.World)
-            .ThenInclude(x => x!.Objects)
+            .ThenInclude(x => x!.Objects.Where(o => o.X == originX && o.Y == originY))
             .FirstOrDefaultAsync(x => x.UserId == userId, cancellationToken);
 
         if (user is null) throw new Exception($"User not found with id {userId}");
 
-        var obj = user.GetWorld().GetBuildingById(request.Building.Id) ?? throw new Exception($"Can't find object with id {request.Building.Id}");
+        var obj = user.GetWorld().GetBuildingByCoord(originX, originY, 0) ?? throw new Exception($"Can't find object at ({originX}, {originY})");
 
         obj.MoveTo(request.Building.Position.X, request.Building.Position.Y, request.Building.Position.Z, request.Building.Direction);
 
@@ -32,11 +36,17 @@ internal sealed class Move(CityVilleDbContext context) : AmfService<MoveRequest>
 public class MoveRequest
 {
     [AmfParam(1)] public MoveBuildingRequest Building { get; set; } = new();
+    [AmfParam(3)] public MoveParamsRequest[] MoveParams { get; set; } = [];
 }
 
 public class MoveBuildingRequest
 {
     [AmfParam("position")] public PerformActionPositionRequest Position { get; set; } = new();
-    [AmfParam("id")] public int Id { get; set; }
     [AmfParam("direction")] public int Direction { get; set; }
+}
+
+public class MoveParamsRequest
+{
+    [AmfParam("origX")] public int OrigX { get; set; }
+    [AmfParam("origY")] public int OrigY { get; set; }
 }
