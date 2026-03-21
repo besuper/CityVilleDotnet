f5abf574e7f529214b0c4c3ed583874149844b10
besuper <16976439+besuper@users.noreply.github.com>
2026-03-21 10:40:04 +0100

    fix: remove invalid cast in PurchaseCrewMember

--- CityVilleDotnet.Api/Services/UserService/PurchaseCrewMember.cs
+++ CityVilleDotnet.Api/Services/UserService/PurchaseCrewMember.cs
@@ -9,9 +9,6 @@ public class PurchaseCrewMember(CityVilleDbContext context) : AmfService
 {
     public override async Task<ASObject> HandlePacket(object[] @params, Guid userId, CancellationToken cancellationToken)
     {
-        var buildingId = (int)@params[0];
-        var typeOfUpgrade = (string)@params[1]; // TODO: implement enum?
-
         // TODO: Implement crew members logic
 
         return GatewayService.CreateEmptyResponse();
