b34204a70a13408d517603b4a43011ef325d84c9
besuper <16976439+besuper@users.noreply.github.com>
2026-03-28 01:18:42 +0100

    refactor: add request & clean code in ProcessVisitsBatch

--- CityVilleDotnet.Api/Services/UserService/ProcessVisitsBatch.cs
+++ CityVilleDotnet.Api/Services/UserService/ProcessVisitsBatch.cs
@@ -1,70 +1,36 @@
 ﻿using CityVilleDotnet.Api.Common.Amf;
 using CityVilleDotnet.Domain.Entities;
-using CityVilleDotnet.Domain.Enums;
 using CityVilleDotnet.Persistence;
 using FluorineFx;
 using Microsoft.EntityFrameworkCore;
 
 namespace CityVilleDotnet.Api.Services.UserService;
 
-internal sealed class ProcessVisitsBatch(CityVilleDbContext context, ILogger<ProcessVisitsBatch> logger) : AmfService
+internal sealed class ProcessVisitsBatch(CityVilleDbContext context) : AmfService<ProcessVisitsBatchRequest>
 {
-    public override async Task<ASObject> HandlePacket(object[] @params, Guid userId, CancellationToken cancellationToken)
+    public override async Task<ASObject> HandlePacket(ProcessVisitsBatchRequest request, Guid userId, CancellationToken cancellationToken)
     {
         // TODO: Add offline simulation
-        var content = @params[0] as ASObject;
-
-        if (content is null) throw new Exception("ProcessVisitsBatch content is null");
-
-        // Key => ID
-        // Value => visits count
         var visits = new Dictionary<int, int>();
 
-        foreach (var (key, value) in content)
+        foreach (var (key, value) in request.Content)
         {
-            if (key is null || value is null) continue;
+            if (!int.TryParse(key, out var id) || value is not ASObject actions)
+                continue;
 
-            if (int.TryParse(key, out var id))
+            foreach (var (_, actionInfo) in actions)
             {
-                var dictValue = value as ASObject;
+                if (actionInfo is not ASObject dict || !dict.TryGetValue("count", out var flatCount))
+                    continue;
 
-                if (dictValue is null) continue;
+                var count = Convert.ToInt32(flatCount);
 
-                foreach (var (action, actionInformations) in dictValue)
-                {
-                    logger.LogDebug($"Add count for ID {id} with ACTION {action}");
-
-                    var dictActionInformations = actionInformations as ASObject;
-
-                    if (dictActionInformations is null)
-                    {
-                        logger.LogDebug($"Action information is not a dictionary?");
-                        continue;
-                    }
-
-                    if (dictActionInformations.TryGetValue("count", out var flatCount))
-                    {
-                        var count = int.Parse(flatCount.ToString());
-
-                        if (visits.ContainsKey(id))
-                        {
-                            visits[id] += count;
-                        }
-                        else
-                        {
-                            visits[id] = count;
-                        }
-                    }
-                    else
-                    {
-                        logger.LogDebug($"Unable to find count inside dict {dictActionInformations}");
-                    }
-                }
+                if (!visits.TryAdd(id, count))
+                    visits[id] += count;
             }
         }
 
         var ids = visits.Keys.ToList();
-        var counts = visits.Values.ToList();
 
         var user = await context.Set<User>()
             .AsSplitQuery()
@@ -74,22 +40,22 @@ internal sealed class ProcessVisitsBatch(CityVilleDbContext context, ILogger<Pro
 
         foreach (var obj in user.GetWorld().Objects)
         {
-            var index = ids.IndexOf(obj.WorldFlatId);
+            var id = ids.Contains(obj.WorldFlatId) ? obj.WorldFlatId
+                : ids.Contains(obj.TempId) ? obj.TempId
+                : -1;
 
-            if (index == -1)
-            {
-                // Not found with current server ID, check with clientId
-                index = ids.IndexOf(obj.TempId);
-            }
+            if (id == -1) continue;
 
-            if (index == -1) continue;
-
-            var newCount = counts[index];
-            obj.UpdateVisits(newCount);
+            obj.UpdateVisits(visits[id]);
         }
 
         await context.SaveChangesAsync(cancellationToken);
 
         return new CityVilleResponse();
     }
+}
+
+public class ProcessVisitsBatchRequest
+{
+    [AmfParam(0)] public ASObject Content { get; set; } = [];
 }
