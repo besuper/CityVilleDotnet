dd7ca20402735429ac468dee15d680791a586707
besuper <16976439+besuper@users.noreply.github.com>
2026-04-02 20:07:04 +0200

    chore: add validator in BuyEnergy

--- CityVilleDotnet.Api/Services/UserService/BuyEnergy.cs
+++ CityVilleDotnet.Api/Services/UserService/BuyEnergy.cs
@@ -4,6 +4,7 @@ using CityVilleDotnet.Common.Settings;
 using CityVilleDotnet.Domain.Entities;
 using CityVilleDotnet.Domain.Enums;
 using CityVilleDotnet.Persistence;
+using FluentValidation;
 using FluorineFx;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,19 +14,17 @@ public class BuyEnergy(CityVilleDbContext context) : AmfService<BuyEnergyRequest
 {
     public override async Task<ASObject> HandlePacket(BuyEnergyRequest request, Guid userId, CancellationToken cancellationToken)
     {
+        var gameItem = GameSettingsManager.Instance.GetItem(request.ItemName);
+
+        if (gameItem is null) throw new Exception($"Game item {request.ItemName} not found");
+        if (gameItem.Cash is null || gameItem.EnergyRewards is null) throw new Exception($"Game item {request.ItemName} doesn't have cash or energy reward");
+
         var player = await context.Set<User>()
             .Where(x => x.UserId == userId)
             .Select(x => x.Player)
             .FirstOrDefaultAsync(cancellationToken);
 
-        if (player is null) throw new Exception("Can't find player with UserId");
-
-        var gameItem = GameSettingsManager.Instance.GetItem(request.ItemName);
-
-        if (gameItem is null) throw new Exception($"Game item {request.ItemName} not found");
-
-        if (gameItem.Cash is null || gameItem.EnergyRewards is null)
-            throw new Exception($"Game item {request.ItemName} does defineds not have cash or energy reward");
+        if (player is null) throw new Exception("Can't find player");
 
         if (player.Cash < gameItem.Cash)
             return new CityVilleResponse().Error(GameErrorType.NotEnoughMoney);
@@ -42,4 +41,12 @@ public class BuyEnergy(CityVilleDbContext context) : AmfService<BuyEnergyRequest
 public class BuyEnergyRequest
 {
     [AmfParam(0)] public string ItemName { get; set; } = string.Empty;
+}
+
+public class BuyEnergyValidator : AbstractValidator<BuyEnergyRequest>
+{
+    public BuyEnergyValidator()
+    {
+        RuleFor(x => x.ItemName).NotEmpty().MaximumLength(64);
+    }
 }
