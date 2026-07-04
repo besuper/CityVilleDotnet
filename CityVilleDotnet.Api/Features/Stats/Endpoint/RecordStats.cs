d078445779c443fe0fd82d8fbb86cd7e10f9790e
besuper <16976439+besuper@users.noreply.github.com>
2026-07-04 11:33:41 +0200

    chore: add tracking in record stats endpoint

--- CityVilleDotnet.Api/Features/Stats/Endpoint/RecordStats.cs
+++ CityVilleDotnet.Api/Features/Stats/Endpoint/RecordStats.cs
@@ -1,8 +1,12 @@
-﻿using FastEndpoints;
+﻿using CityVilleDotnet.Domain.Entities;
+using CityVilleDotnet.Persistence;
+using FastEndpoints;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityVilleDotnet.Api.Features.Stats.Endpoint;
 
-internal sealed class RecordStats : EndpointWithoutRequest
+internal sealed class RecordStats(UserManager<ApplicationUser> userManager, CityVilleDbContext dbContext) : EndpointWithoutRequest
 {
     public override void Configure()
     {
@@ -11,6 +15,26 @@ internal sealed class RecordStats : EndpointWithoutRequest
 
     public override async Task HandleAsync(CancellationToken ct)
     {
+        var currentUser = await userManager.GetUserAsync(HttpContext.User);
+
+        if (currentUser is null)
+        {
+            await Send.OkAsync(cancellation: ct);
+            return;
+        }
+        
+        var player = await dbContext.Set<Player>().FirstOrDefaultAsync(x => x.AppUser!.Id == currentUser.Id, ct);
+        
+        if (player is null)
+        {
+            await Send.OkAsync(cancellation: ct);
+            return;
+        }
+        
+        player.UpdateTracking();
+        
+        await dbContext.SaveChangesAsync(ct);
+        
         await Send.OkAsync(cancellation: ct);
     }
 }
